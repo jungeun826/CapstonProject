6f0f1e05fba6c34e0656c7a0394dc0f6f089e724
YuneeJang <joanaun92@gmail.com>
2015-03-21 13:47:33 +0900

    프로젝트 초기 환경 설정
    ovencv - 2.4.10 + openCSharp
    웹캠 읽어와 보여주는 것 까지 함.

---
+++ GestureRecognitionProject/GestureRecognitionProject/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GestureRecognitionProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 1024);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(161, 13);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 2;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(161, 43);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 3;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+    }
+}
+
---
+++ GestureRecognitionProject/GestureRecognitionProject/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Threading;
+using OpenCvSharp.Extensions;
+//using System.Windows.
+
+namespace GestureRecognitionProject
+{
+    public partial class Form1 : Form
+    {
+        private CvCapture capture;
+        private DispatcherTimer timer;
+        private WriteableBitmap writeBitmap;
+        private IplImage src;
+        //private List<System.Windows.Media.Color> bitmapColors;
+        //private BitmapPalette bitmapPallette;
+            //new BitmapPalette(new List<System.Windows.Media.Color>() { new System.Windows.Media.Color() });
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //bitmapColors = new List<System.Windows.Media.Color>();
+            //bitmapColors.Add(System.Windows.Media.Colors.Red);
+            //bitmapColors.Add(System.Windows.Media.Colors.Blue);
+            //bitmapColors.Add(System.Windows.Media.Colors.Green);
+
+            
+            //bitmapPallette = new BitmapPalette(bitmapColors);
+            
+            
+            
+            //IplImage img = Cv.LoadImage("C:\\capture.png", LoadMode.GrayScale);
+            //Stream inputStream = null;
+            //img.ToStream(inputStream, "", new ImageEncodingParam(ImageEncodingID.JpegQuality, 0));
+            //pictureBox1.Image = new Bitmap(inputStream);
+            ////pictureBox1.Image = 
+
+            ////카메라 지정
+            ////제어판 기준인듯?
+            //m_cvCap = CvCapture.FromCamera(CaptureDevice.DShow,0);
+            //m_cvCap.FrameWidth = 320;
+            //m_cvCap.FrameHeight = 240;
+
+            ////타이머 설정
+            //timer1.Interval = 20;
+            //timer1.Enabled = true;
+
+            // 캠 목록 얻어오기
+            foreach (DirectShowLib.DsDevice ds in DirectShowLib.DsDevice.GetDevicesOfCat(DirectShowLib.FilterCategory.VideoInputDevice))
+            {
+                comboBox1.Items.Add(ds.Name);//.Items.Add(ds.Name);
+            }
+
+            SetTimer();
+        }
+
+        private void SetTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 33);
+            timer.Tick += new EventHandler(timer1_Tick);
+        }
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ////카메라에서 프레임 가져온다.
+            //m_cvImg = m_cvCap.QueryFrame();
+            ////IplImage을 비트맵으로 전환
+            ////pictureBox1.Image = m_cvImg.ToBitmap();
+            //Stream inputStream = null;
+            //m_cvImg.ToStream(inputStream, "", new ImageEncodingParam(ImageEncodingID.JpegQuality, 0));
+            //pictureBox1.Image = new Bitmap(inputStream);
+            // 카메라의 프레임을 비트맵으로 변환
+            using (src = capture.QueryFrame())//RetrieveFrame())//e.QueryFrame())
+            {
+                WriteableBitmapConverter.ToWriteableBitmap(src, writeBitmap);
+                MemoryStream inputStream = new MemoryStream();
+                writeBitmap.ToIplImage().ToStream(inputStream, ".jpeg", new ImageEncodingParam(ImageEncodingID.JpegQuality, 1));
+                pictureBox1.Image = new Bitmap(inputStream);
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Dispose();
+            }
+
+            if (timer != null)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //initCamera(camList.SelectedIndex);
+            try
+            {
+                // 해당 카메라 가져오기
+                capture = CvCapture.FromCamera(0);//CaptureDevice.DShow, comboBox1.SelectedIndex);
+                // 이미지에 비트맵 연결
+                writeBitmap = new WriteableBitmap(capture.FrameWidth, capture.FrameHeight, 96, 96, PixelFormats.Rgb24, null);// BitmapPalettes.WebPaletteTransparent);
+                
+                //writeBitmap = new WriteableBitmap(capture.FrameWidth, capture.FrameHeight, 96, 96, PixelFormats.Bgra32, null);
+                
+                //pictureBox1.Image = writeBitmap.ToIplImage();
+            }
+            catch (Exception exception)
+            {
+                //if (timer != null)
+                //{
+                //    timer.Stop();
+                //}
+
+                if (capture != null)
+                {
+                    capture.Dispose();
+                    capture = null;
+                }
+                MessageBox.Show(exception.ToString());
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (capture != null && !timer.IsEnabled)
+            {
+                timer.IsEnabled = true;
+                timer.Start();
+            }
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                timer.IsEnabled = false;
+                timer.Stop();
+            }
+        }
+    }
+}

354ca12fd14b2f449818ce46884d79fc6e6c2068
kimyonglae <blackdragon01@empas.com>
2015-03-21 19:38:42 +0900

    WebcamController클래스 추가 / Form1의 웹캠관련 코드 분리

--- HandGesture/HandGesture/Form1.cs
+++ HandGesture/HandGesture/Form1.cs
@@ -14,9 +14,6 @@ namespace HandGesture
 {
     public partial class Form1 : Form
     {
-        IplImage m_cvImg;
-        CvCapture m_cvCap;
-
         public Form1()
         {
             InitializeComponent();
@@ -24,13 +21,6 @@ namespace HandGesture
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            //카메라 지정
-            //제어판 기준인듯?
-            m_cvCap = CvCapture.FromCamera(0);
-            m_cvCap.FrameWidth = 3200;
-            m_cvCap.FrameHeight = 2400;
-
             //타이머 설정
             timer1.Interval = 20;
             timer1.Enabled = true;
@@ -38,12 +28,10 @@ namespace HandGesture
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //카메라에서 프레임 가져온다.
-            m_cvImg = m_cvCap.QueryFrame();
-            //IplImage을 비트맵으로 전환
-            pictureBox1.Image = m_cvImg.ToBitmap();
+            WebcamController.updateFrame();
+            pictureBox1.Image = WebcamController.getFrameAsBMP();
 
-            //헤헷
+            
         }
     }
 
---
+++ HandGesture/HandGesture/WebcamController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Drawing;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace HandGesture
+{
+    delegate void updateDelegate();
+
+    /// <summary>
+    /// Webcam이 보고있는 것이 무엇인지 알고싶어요?? 나를 써봐요
+    /// </summary>
+    public static class WebcamController
+    {
+        private static IplImage m_cvImg;
+        private static CvCapture m_cvCap;
+        private static updateDelegate m_updateDel;
+
+        static WebcamController()
+        {
+            //카메라 지정
+            //0번카메라를 사용한다.
+            m_cvCap = CvCapture.FromCamera(0);
+            m_cvCap.FrameWidth = 320;
+            m_cvCap.FrameHeight = 240;
+
+            m_updateDel = null;
+
+#if DEBUG
+            addDisplayFPS();
+#endif
+        }
+
+        /// <summary>
+        /// 현제 프레임을 IplImage형식으로 반환한다.
+        /// </summary>
+        /// <returns>IplImage</returns>
+        public static IplImage getImg()
+        {
+            return m_cvImg;
+        }
+
+        /// <summary>
+        /// 현제 프레임을 Bitmap형식으로 반환한다.
+        /// </summary>
+        /// <returns>Bitmap</returns>
+        public static Bitmap getFrameAsBMP()
+        {
+            return m_cvImg.ToBitmap();
+        }
+
+        /// <summary>
+        /// 수동으로 현재 프레임을 업데이트 한다.
+        /// </summary>
+        public static void updateFrame()
+        {
+            m_cvImg = m_cvCap.QueryFrame();
+            if (m_updateDel != null) m_updateDel();
+        }
+
+#if DEBUG
+        CvFont font;
+
+        private static void addDisplayFPS()
+        {
+            //font = new CvFont( )
+        }
+
+        private static void displayFPS()
+        {
+            //TODO FPS표시하기 구현
+
+        }
+#endif
+    }
+}

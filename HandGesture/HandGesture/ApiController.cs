ad658abdd2f286080b33c838d338fe9cad52da1a
kimyonglae <blackdragon01@empas.com>
2015-04-13 13:15:21 +0900

    ApiController 추가,,,

---
+++ HandGesture/HandGesture/ApiController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; //이벤트 발생위한 네임스페이스
+using System.Windows.Forms; //현재 마우스위치를 위한 네임스페이스
+
+
+namespace HandGesture
+{
+    class ApiController
+    {
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        /// <summary>
+        /// 마우스 이벤트를 발생 시킨다.
+        /// dwData랑 dwExtraInfo는 뭔지 나도 모름요
+        /// </summary>
+        /// <param name="dwFlags"></param>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <param name="dwData"></param>
+        /// <param name="dwExtraInfo"></param>
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData = 0, int dwExtraInfo = 0);
+        
+        /// <summary>
+        /// 마우스커서 위치 변경하는녀석임
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        static extern int SetCursorPos(int x, int y);
+
+        /// <summary>
+        /// 키보드이벤트를 발생시킨다.
+        /// </summary>
+        /// <param name="vk">ex) Keys.Left</param>
+        /// <param name="scan">스캔코드, 그냥 0써서 넘어가면됨</param>
+        /// <param name="flags">0x00, 0x02 각각 누름과 뗌</param>
+        /// <param name="extraInfo">일반키보드이므로 0 쓰세얌</param>
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(uint vk, uint scan, uint flags, uint extraInfo);
+    }
+}

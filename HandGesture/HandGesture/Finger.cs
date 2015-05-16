b3c88a102b7f940f466c471d502fbf5f220b6ac0
JungEun <jungeun826@nate.com>
2015-05-17 03:42:55 +0900

    손가락 클래스 추가

---
+++ HandGesture/HandGesture/Finger.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandGesture
+{
+    class Finger
+    {
+        [Flags]
+        public enum FingerType
+        {
+            ForeFinger,
+            IndexFinger,
+            MiddleFinger,
+            RingFinger,
+            LittleFinger,
+        }
+
+        private int _fingerCnt = 0;
+        public int FingerCnt { get { return _fingerCnt; } }
+
+        private FingerType _fingerPos;
+        public FingerType FingerPos { get { return _fingerPos; } }
+    }
+}
